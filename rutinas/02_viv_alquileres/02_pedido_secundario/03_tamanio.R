dd012adb5110d37dfd0b2f2903142fa4f4bcca30
angelomar593 <omarpbrasta@hotmail.com>
2025-07-07 21:15:39 -0500

    pedido dos tamano

--- rutinas/02_viv_alquileres/02_pedido_secundario/03_tamanio.R
+++ rutinas/02_viv_alquileres/02_pedido_secundario/03_tamanio.R
@@ -9,7 +9,7 @@ source("rutinas/99_librerias_funciones/99_mult_6.R")
 #-------------------------------------------------------------------------------
 
 #est <- readRDS("productos/02_viv_alquileres/02_pedido_secundario/02_estimaciones/estimaciones.rds")
-est <- readRDS("productos/02_viv_alquileres/02_pedido_secundario/02_estimaciones/estimaciones_1_2.rds")
+est <- readRDS("productos/02_viv_alquileres/02_pedido_secundario/02_estimaciones/estimaciones.rds")
 
 # -----------------------------------------------------------------------------
 # Lectura marco que viene del censo
@@ -19,15 +19,15 @@ ruta <- "productos/02_viv_alquileres/02_pedido_secundario/01_marco/marco_ipc_alq
 marco_ipc_alquileres_2025 <- import(ruta)
 
 #-------------------------------------------------------------------------------
-# Tamaño para los dominios sin considerar Galápagos
+# Tamaño para los dominios sin considerar Galápagos ni Esmeraldas
 #-------------------------------------------------------------------------------
 
 nc = 0.95
 z = qnorm(nc + (1-nc)/2)
-er = 0.1
+er = 0.05
 
 tamanio_1 <- est %>% 
-  filter(dominio != "20") %>% 
+  filter(dominio != "20" & dominio != "080150") %>% 
   group_by(dominio, nombre_dom) %>% 
   summarise(N = N_alq_censo,
             n = unique(n),
@@ -35,11 +35,36 @@ tamanio_1 <- est %>%
             y = med_alq) %>% 
   mutate(numerador = (desv)^2,
          denominador = ((er*y/z)^2) + (desv^2/N),
-         tam = ceiling(numerador/denominador), 
-         tam = mult_6(tam, N)) %>% 
+         tam = ceiling(numerador/denominador)) %>% 
+         #tam = mult_6(tam, N)) %>% 
   select(dominio, nombre_dom, tam)
 
-tamanio <- tamanio_1
+#-------------------------------------------------------------------------------
+# Tamaño solo para ESMERALDAS
+#-------------------------------------------------------------------------------
+
+nc = 0.95
+z = qnorm(nc + (1-nc)/2)
+er = 0.065
+
+tamanio_2 <- est %>% 
+  filter(dominio == "080150") %>% 
+  group_by(dominio, nombre_dom) %>% 
+  summarise(N = N_alq_censo,
+            n = unique(n),
+            desv = sd_alq,
+            y = med_alq) %>% 
+  mutate(numerador = (desv)^2,
+         denominador = ((er*y/z)^2) + (desv^2/N),
+         tam = ceiling(numerador/denominador)) %>% 
+         #tam = mult_6(tam, N)) %>% 
+  select(dominio, nombre_dom, tam)
+
+#-------------------------------------------------------------------------------
+# Tamanio Fianl
+#-------------------------------------------------------------------------------
+
+tamanio <- rbind(tamanio_1, tamanio_2) 
 
 #-------------------------------------------------------------------------------
 # Distribución tamaño para CASA (01) y DEPARTAMENTO (02)
@@ -52,16 +77,27 @@ tamanio <- marco_ipc_alquileres_2025 %>%
   mutate(prop = n_casas_dep / sum(n_casas_dep) ) %>%
   ungroup() %>% 
   left_join(select(tamanio, tam, id_dom = dominio, nombre_dom)) %>% 
-  mutate(tam_distr = ceiling(tam * prop)) 
+  mutate(tam_distr = ceiling(tam * prop), 
+         tam_distr = mult_6(tam_distr, n_casas_dep)) %>% 
+  filter(id_dom != "20")
 
 #tamanio_01 <- readRDS("productos/02_viv_alquileres/01_pedido_inicial/03_tamanio/tamanio.rds")
+
+if(tamanio %>% group_by(id_dom) %>% 
+   summarise(s_p = sum(prop)) %>% 
+   select(s_p) %>% unique() == 1){
+  print("Proporciones correctas")
+}else{
+  message("Error en proporciones")
+}
 
 #-------------------------------------------------------------------------------
 # Exportando
 #-------------------------------------------------------------------------------
 
 ruta <- "productos/02_viv_alquileres/02_pedido_secundario/03_tamanio/"
-export(tamanio, paste0(ruta, "tamanio_pedido_002_casas_0.10_001.xlsx"))
+export(tamanio %>% select(id_dom, nombre_dom, V01, tam_distr), 
+       paste0(ruta, "tam_ped_002_final.rds"))
 
 
 
--- rutinas/02_viv_alquileres/02_pedido_secundario/04_seleccion.R
+++ rutinas/02_viv_alquileres/02_pedido_secundario/04_seleccion.R
@@ -7,42 +7,41 @@ source("rutinas/99_librerias_funciones/librerias.R")
 # CARGAMOS LOS TAMAÑOS ---------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-tamanio <- readRDS("productos/02_viv_alquileres/03_tamanio/tamanio.rds")
+tamanio <- readRDS("productos/02_viv_alquileres/02_pedido_secundario/03_tamanio/tam_ped_002_final.rds")
 
 # -----------------------------------------------------------------------------
 # Lectura marco que viene del censo
 # -----------------------------------------------------------------------------
 
-marco_ipc_alquileres_2025 <- import("productos/02_viv_alquileres/01_marco/marco_ipc_alquileres_2025.rds") 
+marco_ipc_alquileres_2025 <- import("productos/02_viv_alquileres/02_pedido_secundario/01_marco/marco_ipc_alquileres_2025.rds") 
+aux1 <- dim(marco_ipc_alquileres_2025)[1]
 
 # -----------------------------------------------------------------------------
 # Agregando tamaño muestral al marco
 # -----------------------------------------------------------------------------
 
 marco_ipc_alquileres_2025 <- marco_ipc_alquileres_2025 %>% 
-  left_join(select(tamanio, dominio, tam), by = "dominio")
+  left_join(select(tamanio, id_dom, V01, tam_distr), by = c("id_dom", "V01"))
 
+if(aux1 == dim(marco_ipc_alquileres_2025)[1]){
+  print("left join correcto")
+}else{
+  print("left jpin fallido")
+}
 
 # -----------------------------------------------------------------------------
 # Seleccion
 # -----------------------------------------------------------------------------
 
-set.seed(as.numeric(20250414)) 
+set.seed(as.numeric(20250704)) 
 
 muestra_alq_1 <- marco_ipc_alquileres_2025 %>% 
-  filter(dominio != "20") %>% 
-  group_by(dominio) %>% 
-  sample_n(unique(tam)) 
+  filter(id_dom != "20") %>% 
+  group_by(id_dom) %>% 
+  sample_n(unique(tam_distr)) 
 
-muestra_alq_2 <- marco_ipc_alquileres_2025 %>% 
-  filter(dominio == "20") %>% 
-  left_join(select(tamanio_galapagos, dominio, I02, V01, tam_ppt), 
-            by = c("dominio", "I02", "V01" )) %>% 
-  group_by(dominio, I02, V01) %>% 
-  sample_n(unique(tam_ppt)) %>% 
-  select(-tam_ppt)
 
-muestra_alq <- rbind(muestra_alq_1, muestra_alq_2)
+muestra_alq <- muestra_alq_1
 
 if((muestra_alq %>% group_by(dominio, tam) %>% summarise(selec = n()) %>% 
   mutate(control = ifelse(tam == selec, 1, 0)) %>% 
